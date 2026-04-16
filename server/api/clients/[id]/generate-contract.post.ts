ff3a9489349a86b7f07a4d091f72b9ed0c991666
gado77 <vi.emanoel20152015@gmail.com>
2026-04-16 03:39:33 -0300

    fix: correct import path for generate-contract API

--- server/api/clients/[id]/generate-contract.post.ts
+++ server/api/clients/[id]/generate-contract.post.ts
@@ -1,4 +1,4 @@
-import { useDb, generateId } from '../../utils/db'
+import { useDb, generateId } from '../../../utils/db'
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
